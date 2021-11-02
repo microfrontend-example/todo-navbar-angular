7bda02d240158352a4e170771638f662d437c1aa
Carlos Morales <carlosm@youforce.net>
2021-11-01 19:53:21 -0600

    add single spa angular files and modifications

---
+++ extra-webpack.config.js
@@ -0,0 +1,20 @@
+// const singleSpaAngularWebpack =
+//   require("single-spa-angular/lib/webpack").default;
+
+// module.exports = (config, options) => {
+//   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
+
+//   singleSpaWebpackConfig.externals.push("@visma/auth");
+//   // Feel free to modify this webpack config however you'd like to
+//   return singleSpaWebpackConfig;
+// };
+
+const singleSpaAngularWebpack =
+  require("single-spa-angular/lib/webpack").default;
+
+module.exports = (config, options) => {
+  const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
+
+  // singleSpaWebpackConfig.externals.push(/^@visma\/.+$/);
+  return singleSpaWebpackConfig;
+};
