3fff49b3845f08940f1f2f304a07a748de5bb8c2
Carlos Morales <carlosm@youforce.net>
2021-11-01 19:54:38 -0600

    title consuming shared service from @visma/auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { nameApp$ } from '@visma/auth';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-navbar';
+  title = 'navbar';
+  subscription!: Subscription;
+
+  ngOnInit(): void {
+    this.subscription = nameApp$.subscribe((value: string) => {
+      this.title = value;
+    });
+  }
 }
